8acbc6377c5a059fb1bb08b37cd26d7755c545ac
Ewan Westcott Graham <ewg27@open.ac.uk>
2024-11-27 15:42:03 +0000

    Install and use cdk angular component for drag/drop

--- Todo.UI/src/app/app.component.ts
+++ Todo.UI/src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faXmark, faCheck } from '@fortawesome/free-solid-svg-icons';
+import { DragDropModule } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FontAwesomeModule],
+  imports: [RouterOutlet, FontAwesomeModule, DragDropModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
