1b1290c41f89714a47f7d0fee6d809f44c1e3937
Ewan Westcott Graham <ewg27@open.ac.uk>
2024-11-27 16:01:13 +0000

    Implement draggable list items

--- Todo.UI/src/app/app.component.ts
+++ Todo.UI/src/app/app.component.ts
@@ -3,6 +3,7 @@ import { RouterOutlet } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faXmark, faCheck } from '@fortawesome/free-solid-svg-icons';
 import { DragDropModule } from '@angular/cdk/drag-drop';
+import { CdkDragDrop, CdkDropList, CdkDrag, moveItemInArray } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-root',
@@ -14,6 +15,10 @@ export class AppComponent {
   title = 'Todo.UI';
   faXmark = faXmark;
   faCheck = faCheck;
+  items = ['Item 1', 'Item 2', 'Item 3', 'Item 4'];
+
+  drop(event: CdkDragDrop<string[]>) {
+    moveItemInArray(this.items, event.previousIndex, event.currentIndex);
+  }
 }
 
-Function
