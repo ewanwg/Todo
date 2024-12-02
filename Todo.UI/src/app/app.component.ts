11ee70d0758c6d338856b68493530aca8e84a401
Ewan Westcott Graham <ewg27@open.ac.uk>
2024-12-02 08:44:20 +0000

    Implement linethrough and array deletion

--- Todo.UI/src/app/app.component.ts
+++ Todo.UI/src/app/app.component.ts
@@ -4,10 +4,11 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faXmark, faCheck } from '@fortawesome/free-solid-svg-icons';
 import { DragDropModule } from '@angular/cdk/drag-drop';
 import { CdkDragDrop, CdkDropList, CdkDrag, moveItemInArray } from '@angular/cdk/drag-drop';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FontAwesomeModule, DragDropModule],
+  imports: [RouterOutlet, FontAwesomeModule, DragDropModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
@@ -15,10 +16,22 @@ export class AppComponent {
   title = 'Todo.UI';
   faXmark = faXmark;
   faCheck = faCheck;
-  items = ['Item 1', 'Item 2', 'Item 3', 'Item 4'];
+  items = [
+    { text: 'Item 1', completed: false },
+    { text: 'Item 2', completed: false },
+    { text: 'Item 3', completed: false },
+  ];
 
   drop(event: CdkDragDrop<string[]>) {
     moveItemInArray(this.items, event.previousIndex, event.currentIndex);
+  }
+
+  toggleComplete(index: number) {
+    this.items[index].completed = !this.items[index].completed;
+  }
+
+  deleteItem (index:number) {
+    delete this.items[index]
   }
 }
 
