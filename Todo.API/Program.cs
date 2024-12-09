7740062de9379d19d12c1dbba2d2afdda579b1cd
Ewan Westcott Graham <ewg27@open.ac.uk>
2024-12-09 15:23:57 +0000

    Added Json serialiser in Program.cs to avoid the circular reference

--- Todo.API/Program.cs
+++ Todo.API/Program.cs
@@ -14,6 +14,12 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<TodoContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.Preserve;
+    });
+
 
 var app = builder.Build();
 
